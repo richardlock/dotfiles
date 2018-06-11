54acdc4d5cf37fe27a5e40a766be6dad2178dd61
richardlock <richardlock@users.noreply.github.com>
2018-06-11 08:39:12 +0100

    Added quotes to script

--- scripts/Install-WSL.ps1
+++ scripts/Install-WSL.ps1
@@ -1,20 +1,20 @@
 ﻿# Enable Windows Subsystem for Linux
-If ((Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux).State -eq "Disabled") {
-  Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+If ((Get-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux").State -eq "Disabled") {
+  Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux"
 }
 
 # Download Ubuntu archive
-If (!(Test-Path -Path ~\Ubuntu.zip)) {
-  Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile ~/Ubuntu.zip -UseBasicParsing
+If (!(Test-Path -Path "~\Ubuntu.zip")) {
+  Invoke-WebRequest -Uri "https://aka.ms/wsl-ubuntu-1604" -OutFile "~/Ubuntu.zip"
 }
 
 # Extract Ubuntu archive
-If (!(Test-Path -Path ~\Ubuntu)) {
-  Expand-Archive ~\Ubuntu.zip ~\Ubuntu
+If (!(Test-Path -Path "~\Ubuntu")) {
+  Expand-Archive "~\Ubuntu.zip" "~\Ubuntu"
 }
 
 # Initialise Ubuntu
-Start-Process -FilePath ~\Ubuntu\ubuntu.exe -Wait
+Start-Process -FilePath "~\Ubuntu\ubuntu.exe" -Wait
 
 # Run configure script
 Start-Process -FilePath wsl -ArgumentList 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/richardlock/dotfiles/wsl/scripts/configure.sh)"' -Wait
